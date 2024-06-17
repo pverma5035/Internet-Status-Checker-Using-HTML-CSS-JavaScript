5393b3e056218052cf77b3727248f8a0ea7cf9c2
pverma5035 <pverma5035@gmail.com>
2024-06-17 16:15:27 +0530

    first-commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+window.addEventListener("load",checkInternetConnection);
+
+function checkInternetConnection(){
+    const statusText = document.getElementById("statusText");
+    const ipAddressText = document.getElementById("ipAddressText");
+    const networkStrengthText = document.getElementById("networkStrengthText");
+    statusText.textContent="Checking...";
+    if(navigator.onLine){
+        fetch("https://api.ipify.org?format=json")
+        .then((response)=>response.json())
+        .then((data)=>{
+            ipAddressText.textContent=data.ip;
+            statusText.textContent ="Connected"
+            const connection=navigator.connection;
+            const networkStrength=connection?connection.downlink+"MBPS":"Unknown";
+            networkStrengthText.textContent=networkStrength;
+
+        })
+
+
+        .catch(() =>{
+            statusText.textContent="Disconnected";
+            ipAddressText.textContent='-';
+            networkStrengthText='-';
+        });
+    }
+    else{
+        statusText.textContent="Disconnected";
+        ipAddressText.textContent='-';
+        networkStrengthText='-';
+    }
+
+
+}
